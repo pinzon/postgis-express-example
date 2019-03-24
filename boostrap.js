4b1a0f376e21d1e48b012f5c6e8777d28da379e0
Cristopher K. Pinzón Resendiz <cristopher.pinzon@gmail.com>
2019-03-23 21:09:14 -0500

    agebs update

--- boostrap.js
+++ boostrap.js
@@ -1,47 +1,54 @@
 const fs = require("fs");
-const { Pool, Client } = require('pg')
+const { Pool, Client } = require("pg");
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
+  user: "postgres",
+  host: "localhost",
+  database: "postgres",
   // password: 'secretpassword',
-  port: 5432,
-})
+  port: 5432
+});
 
 const readGeoJson = function() {
   console.log("Reading file");
-  var content = fs.readFileSync("./countries.geojson", "utf8");
+  var content = fs.readFileSync("./docs/qroo.geojson", "utf8");
   console.log("Parsing to json");
   var countriesJson = JSON.parse(content);
   return countriesJson;
 };
 
 const getDataFromRow = function(row) {
-  var name = row.properties.name;
-  var type = (row.geometry.type == "Polygon")? 0 :1;
-  var geometry = JSON.stringify(row.geometry);
-  return { name, type, geometry };
+  var ent = parseInt(row.properties.CVE_ENT);
+  var loc = parseInt(row.properties.CVE_LOC);
+  var mun = parseInt(row.properties.CVE_MUN);
+  var ageb = row.properties.CVE_AGEB;
+  var ageb_complete =
+    row.properties.CVE_ENT +
+    row.properties.CVE_MUN +
+    row.properties.CVE_LOC +
+    row.properties.CVE_AGEB;
+  var geom = row.geometry;
+  return { ent, loc, mun, ageb, ageb_complete, geom };
 };
 
-function getQuery(data){
-  var query = `insert into world.country ("geom_${(data.type == 0)? 'pol' : 'mul'}", "name", "type") values (ST_geomFromGeoJSON($1), $2, $3) returning *`
-  return query 
+function getQuery() {
+  // var query = `insert into world.country ("geom", "name", "type") values (ST_geomFromGeoJSON($1), $2, $3) returning *`;
+  var query = "INSERT INTO world.ageb (ent, loc, mun, ageb, ageb_complete, geom) VALUES($1, $2, $3, $4, $5, ST_geomFromGeoJSON($6));"
+  return query;
 }
 
 async function insertCountry(data) {
-  var sql = getQuery(data);
-  var params =  [data.geometry, data.name, data.type]
-  // console.log(sql, params)
-  const res = await pool.query(sql,params)
+  var sql = getQuery();
+  var params = [data.ent, data.loc, data.mun, data.ageb, data.ageb_complete, data.geom];
+  const res = await pool.query(sql, params);
 }
 
 exports.start = async function() {
   var countries = readGeoJson();
   for (let index = 0; index < countries.features.length; index++) {
-    console.log('\nInserting:', index+1 )
+    console.log("\nInserting:", index + 1);
     var data = getDataFromRow(countries.features[index]);
     await insertCountry(data);
   }
-  await pool.end()
+  await pool.end();
 };
--- search.js
+++ search.js
@@ -11,10 +11,10 @@ const pool = new Pool({
 
 exports.searchCountry = async function(lat, lon) {
   console.log(`Searchin ${lat}, ${lon}`);
-  var sql = "SELECT id,name ";
-  sql += "FROM world.country ";
+  var sql = "SELECT id, ageb_complete as name ";
+  sql += "FROM world.ageb ";
   sql +=
-    " where st_covers(geom_mul, ST_GeomFromText($1,4326))  or st_covers(geom_pol, ST_GeomFromText($1,4326)) ";
+    " where st_covers(geom, ST_GeomFromText($1,4326)) ";
   try {
     var res = await pool.query(sql, [`POINT(${lon} ${lat})`]); //lat and lon go in reverse order
   } catch (error) {
